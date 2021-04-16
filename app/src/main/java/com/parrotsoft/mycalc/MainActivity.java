853c84518b34687f971c73424353c6430b2e1667
mlopezariza <mlopez@transportationamerica.com>
2021-04-15 20:09:22 -0500

    feat: agregando validaciones y estilos para que la interfaz sea mas agradable

--- app/src/main/java/com/parrotsoft/mycalc/MainActivity.java
+++ app/src/main/java/com/parrotsoft/mycalc/MainActivity.java
@@ -1,8 +1,10 @@
 package com.parrotsoft.mycalc;
 
+import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.text.InputType;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -75,69 +77,79 @@ public class MainActivity extends AppCompatActivity implements View.OnClickListe
 
         this.btnSin = findViewById(R.id.btnSin);
         btnSin.setOnClickListener(this);
+
+        this.EdResult.setInputType(InputType.TYPE_NULL);
     }
 
     @Override
     public void onClick(View v) {
-        switch (v.getId()) {
-            case R.id.btn0:
-                this.textNumber += "0";
-                break;
-            case R.id.btn1:
-                this.textNumber += "1";
-                break;
-            case R.id.btn2:
-                this.textNumber += "2";
-                break;
-            case R.id.btn3:
-                this.textNumber += "3";
-                break;
-            case R.id.btn4:
-                this.textNumber += "4";
-                break;
-            case R.id.btn5:
-                this.textNumber += "5";
-                break;
-            case R.id.btn6:
-                this.textNumber += "6";
-                break;
-            case R.id.btn7:
-                this.textNumber += "7";
-                break;
-            case R.id.btn8:
-                this.textNumber += "8";
-                break;
-            case R.id.btn9:
-                this.textNumber += "9";
-                break;
-            case R.id.btnAc:
-                this.textNumber = "";
-                break;
-            case R.id.btnPor:
-                this.textNumber += "*";
-                break;
-            case R.id.btnMenos:
-                this.textNumber += "-";
-                break;
-            case R.id.btnMas:
-                this.textNumber += "+";
-                break;
-            case R.id.btnIgual:
-                this.textNumber = Double.toString(Helpers.eval(this.textNumber));
-                break;
-            case R.id.btnDiv:
-                this.textNumber += "/";
-                break;
-            case R.id.btnComa:
-                this.textNumber += ".";
-                break;
-            case R.id.btnCos:
-                this.textNumber += "cos";
-                break;
-            case R.id.btnSin:
-                this.textNumber += "sin";
-                break;
+        try {
+            switch (v.getId()) {
+                case R.id.btn0:
+                    this.textNumber += "0";
+                    break;
+                case R.id.btn1:
+                    this.textNumber += "1";
+                    break;
+                case R.id.btn2:
+                    this.textNumber += "2";
+                    break;
+                case R.id.btn3:
+                    this.textNumber += "3";
+                    break;
+                case R.id.btn4:
+                    this.textNumber += "4";
+                    break;
+                case R.id.btn5:
+                    this.textNumber += "5";
+                    break;
+                case R.id.btn6:
+                    this.textNumber += "6";
+                    break;
+                case R.id.btn7:
+                    this.textNumber += "7";
+                    break;
+                case R.id.btn8:
+                    this.textNumber += "8";
+                    break;
+                case R.id.btn9:
+                    this.textNumber += "9";
+                    break;
+                case R.id.btnAc:
+                    this.textNumber = "";
+                    break;
+                case R.id.btnPor:
+                    this.textNumber += "*";
+                    break;
+                case R.id.btnMenos:
+                    this.textNumber += "-";
+                    break;
+                case R.id.btnMas:
+                    this.textNumber += "+";
+                    break;
+                case R.id.btnIgual:
+                    this.textNumber = Double.toString(Math.round(Helpers.eval(this.textNumber) * 100d) / 100d);
+                    break;
+                case R.id.btnDiv:
+                    this.textNumber += "/";
+                    break;
+                case R.id.btnComa:
+                    this.textNumber += ".";
+                    break;
+                case R.id.btnCos:
+                    this.textNumber += "cos";
+                    break;
+                case R.id.btnSin:
+                    this.textNumber += "sin";
+                    break;
+            }
+            this.EdResult.setText(this.textNumber);
+        } catch (Exception e) {
+            AlertDialog.Builder builder = new AlertDialog.Builder(this);
+            builder.setMessage("Por favor verificar la operacion digitada")
+                    .setTitle("Verificar los valores")
+                    .create()
+                    .show();
         }
-        this.EdResult.setText(this.textNumber);
     }
 }
